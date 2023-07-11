6791e1ea071b8acd092d9feaa79d15fb5d939a5a
anastasia.miheeva <a.miheeva@labirint-t.ru>
2023-07-11 16:22:44 +0300

    fix footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,22 +1,14 @@
 <template>
-  <footer class="footer" :class="{ 'text-bg': activeRoute($route.name) }">
+  <footer class="footer">
     <div class="container flex">
       <div class="footer__link">2023</div>
-      <a href="https://www.infografika.agency/">Сделано в Инфографике</a>
+      <a target="_blank" href="https://www.infografika.agency/">Сделано в Инфографике</a>
     </div>
   </footer>
 </template>
 
 <script>
-export default {
-  methods: {
-    activeRoute(route) {
-      if (route === 'main') {
-        return true
-      }
-    }
-  }
-}
+export default {}
 </script>
 
 <style>
@@ -24,11 +16,6 @@ export default {
   color: #052233;
   font-size: 1.11vw;
   line-height: 1.67vw;
-}
-
-.footer.text-bg {
-  color: #fff;
-  background-color: #5BADDE;
 }
 
 .footer .container {
